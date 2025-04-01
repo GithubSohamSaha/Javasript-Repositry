e5854c309c8d9bf01ffd79db1fd53a97955399d8
Soham Saha <144131050+GithubSohamSaha@users.noreply.github.com>
2025-04-01 12:54:37 +0000

    Datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict"; // this is used to treat all JS code as ES6, newe version of JS, this is not mandatory but it is a good practice to use it
+
+//bonus :::
+alert(3+4) // this is used to print the output in the browser, but using node.js alert can not be used
+
+
+
+//types of datatypes in Javascript
+//number - can store numbers. range -2^53 to 2^53
+//float - can store decimal numbers. range -2^53 to 2^53
+//string - can store string values. range -2^53 to 2^53
+//boolean - can store true or false values.
+//undefined - can store undefined values. 
+//null - can store null values. A stand-alone value that represents the absence of any value.it's type is object.
+//symbol - can store unique values.Maximum used in React.js for unique keys.
+//bigint - can store big integers. range -2^53 to 2^53
+
